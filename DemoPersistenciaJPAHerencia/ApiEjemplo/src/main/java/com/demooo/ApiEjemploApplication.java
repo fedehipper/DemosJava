d5b133d6fcc766908e1fab3233bbfca671f61c59
Hipper <Hipper@DESKTOP-VVCT6IJ>
2018-03-17 02:02:02 -0300

    pasa covertura

--- DemoPersistenciaJPAHerencia/ApiEjemplo/src/main/java/com/demooo/ApiEjemploApplication.java
+++ DemoPersistenciaJPAHerencia/ApiEjemplo/src/main/java/com/demooo/ApiEjemploApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiEjemploApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiEjemploApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEjemploApplication.class, args);
+    }
 }
--- DemoPersistenciaJPAHerencia/ApiEjemplo/src/test/java/com/demooo/service/PersonaServiceTest.java
+++ DemoPersistenciaJPAHerencia/ApiEjemplo/src/test/java/com/demooo/service/PersonaServiceTest.java
@@ -5,6 +5,7 @@ import com.demooo.domain.Aldeano;
 import com.demooo.domain.Guerrero;
 import com.demooo.domain.Pueblo;
 import com.demooo.domain.Tipo;
+import com.demooo.excepcion.ErrorException;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
@@ -13,26 +14,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class PersonaServiceTest extends ApiEjemploApplicationTests {
 
     @Autowired
-    AldeanoService aldeanoService;
+    private AldeanoService aldeanoService;
 
     @Autowired
-    GuerreroService guerreroService;
+    private GuerreroService guerreroService;
+
+    @Autowired
+    private PersonaService personaService;
 
     @Test
     public void buscarTodos_conAldeanos_devuelveTodosLosAldeanos() {
         List<Aldeano> aldeanos = aldeanoService.buscarTodos();
-        
+
         assertThat(new Aldeano("julieta", "granola", 20, Tipo.ALDEANO, new Pueblo("lomas", 1000), "tomates"))
                 .isEqualToComparingOnlyGivenFields(aldeanos.get(0));
     }
-    
+
     @Test
     public void buscarPorId_conAldeanoExistente_retornaAldeanoConEseId() {
         Aldeano aldeano = aldeanoService.buscarPorId(3L).get();
         assertThat(new Aldeano("julieta", "granola", 20, Tipo.ALDEANO, new Pueblo("lomas", 1000), "tomates"))
                 .isEqualToComparingOnlyGivenFields(aldeano);
     }
-    
+
     @Test
     public void buscarPorId_conGuerreroExistente_retornaGuerreroConEseId() {
         Guerrero guerrero = guerreroService.buscarPorId(1L).get();
@@ -47,6 +51,46 @@ public class PersonaServiceTest extends ApiEjemploApplicationTests {
                 .isEqualToComparingOnlyGivenFields(guerreros.get(0));
         assertThat(new Guerrero("juan", "garcia", 25, Tipo.GUERRERO, new Pueblo("lomas", 1000), "cortar brazos", 2))
                 .isEqualToComparingOnlyGivenFields(guerreros.get(1));
+    }
+
+    @Test
+    public void buscarPorId_conAldeanoYGuerreroExistentes_devuelvenAldeanoYGuerreroConEseId() {
+        Aldeano aldeano = (Aldeano) personaService.buscarPorId(3L);
+        Guerrero guerrero = (Guerrero) personaService.buscarPorId(1L);
+        assertThat(new Aldeano("julieta", "granola", 20, Tipo.ALDEANO, new Pueblo("lomas", 1000), "tomates"))
+                .isEqualToComparingOnlyGivenFields(aldeano);
+        assertThat(new Guerrero("carlos", "calvo", 22, Tipo.GUERRERO, new Pueblo("lomas", 1000), "cortar cabezas", 1))
+                .isEqualToComparingOnlyGivenFields(guerrero);
+    }
+
+    @Test
+    public void buscarPorTipoPersonaYIdPersonsa_conAldeanoExistentes_devuelvenAldeanoYGuerreroConEseId() {
+        Aldeano aldeano = (Aldeano) personaService.buscarPorTipoPersonaYIdPersonsa("aldeanos", 3L);
+        Guerrero guerrero = (Guerrero) personaService.buscarPorTipoPersonaYIdPersonsa("guerreros", 1L);
+        assertThat(new Aldeano("julieta", "granola", 20, Tipo.ALDEANO, new Pueblo("lomas", 1000), "tomates"))
+                .isEqualToComparingOnlyGivenFields(aldeano);
+        assertThat(new Guerrero("carlos", "calvo", 22, Tipo.GUERRERO, new Pueblo("lomas", 1000), "cortar cabezas", 1))
+                .isEqualToComparingOnlyGivenFields(guerrero);
+    }
+
+    @Test(expected = ErrorException.class)
+    public void buscarPorId_conGuerreroInexistente_lanzaExcepcion() {
+        personaService.buscarPorId(9999L);
+    }
+    
+    @Test(expected = ErrorException.class)
+    public void buscarPorTipoPersonaYIdPersonsa_conTipoGuerrerosExisteYIdNo_lanzaExcepcion() {
+        personaService.buscarPorTipoPersonaYIdPersonsa("guerreros", 999L);
+    }
+    
+    @Test(expected = ErrorException.class)
+    public void buscarPorTipoPersonaYIdPersonsa_conTipoAldeanosExisteYIdNo_lanzaExcepcion() {
+        personaService.buscarPorTipoPersonaYIdPersonsa("aldeanos", 999L);
+    }
+    
+    @Test(expected = ErrorException.class)
+    public void buscarPorTipoPersonaYIdPersonsa_conTipoPersonaNoExisteYIdNo_lanzaExcepcion() {
+        personaService.buscarPorTipoPersonaYIdPersonsa("saraza", 999L);
     }
 
 }
